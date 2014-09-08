c9f13939d36c292140b89b6f2d342908f5dd873f
Randy LeVeque <rjl@uw.edu>
2014-09-08 13:07:10 -0400

    modified all make_fgmax_grid.py files to use new fgmax_tools from geoclaw@aa353b03aa6164

--- Runs/HAI1107/make_fgmax_grid.py
+++ Runs/HAI1107/make_fgmax_grid.py
@@ -2,26 +2,27 @@
 Create fgmax_grid.txt input file
 """
 
-#import fgmax_tools as F   # requires WAcoast/python_tools on path
-from clawpack.geoclaw import fgmax_tools as F
+from clawpack.geoclaw import fgmax_tools
 
 
 def make_fgmax_grid1():
-    FG = F.fgmax_grid_parameters()
-    FG.fname = 'fgmax1.txt'
-    FG.point_style = 2       # will specify a 2d grid of points
-    FG.x1 = 202.08
-    FG.x2 = 202.14
-    FG.y1 = 21.28
-    FG.y2 = 21.34
-    FG.dx = 1./(3600.)    # 1 arcssecond grid
-    FG.tstart_max = 7.5*3600.     # when to start monitoring max values
-    FG.tend_max = 1.e10       # when to stop monitoring max values
-    FG.dt_check = 30.         # target time (sec) increment between updating 
+    fg = fgmax_tools.fgmax()
+    fg.point_style = 2       # will specify a 2d grid of points
+    fg.x1 = 202.08
+    fg.x2 = 202.14
+    fg.y1 = 21.28
+    fg.y2 = 21.34
+    fg.dx = 1./(3600.)    # 1 arcssecond grid
+    fg.tstart_max = 7.5*3600.     # when to start monitoring max values
+    fg.tend_max = 1.e10       # when to stop monitoring max values
+    fg.dt_check = 30.         # target time (sec) increment between updating 
                                # max values
-    FG.min_level_check = 6    # which levels to monitor max on
-    FG.arrival_tol = 1.e-2    # tolerance for flagging arrival
-    F.make_fgmax(FG)
+    fg.min_level_check = 6    # which levels to monitor max on
+    fg.arrival_tol = 1.e-2    # tolerance for flagging arrival
+
+    fg.input_file_name = 'fgmax1.txt'
+    fg.write_input_data()
+
 
 
 if __name__ == "__main__":
--- Runs/HAI1116-18-19-20-21-22/make_fgmax_grid.py
+++ Runs/HAI1116-18-19-20-21-22/make_fgmax_grid.py
@@ -2,26 +2,26 @@
 Create fgmax_grid.txt input file
 """
 
-#import fgmax_tools as F   # requires WAcoast/python_tools on path
-from clawpack.geoclaw import fgmax_tools as F
+from clawpack.geoclaw import fgmax_tools
 
 
 def make_fgmax_grid1():
-    FG = F.fgmax_grid_parameters()
-    FG.fname = 'fgmax1.txt'
-    FG.point_style = 2       # will specify a 2d grid of points
-    FG.x1 = 202.6
-    FG.x2 = 204.2
-    FG.y1 = 20.4
-    FG.y2 = 21.4
-    FG.dx = 1./(240.)    # 15 arcssecond grid
-    FG.tstart_max =  6.5*3600.     # when to start monitoring max values
-    FG.tend_max = 1.e10       # when to stop monitoring max values
-    FG.dt_check = 15.         # target time (sec) increment between updating 
+    fg = fgmax_tools.fgmax()
+    fg.point_style = 2       # will specify a 2d grid of points
+    fg.x1 = 202.6
+    fg.x2 = 204.2
+    fg.y1 = 20.4
+    fg.y2 = 21.4
+    fg.dx = 1./(240.)    # 15 arcssecond grid
+    fg.tstart_max =  6.5*3600.     # when to start monitoring max values
+    fg.tend_max = 1.e10       # when to stop monitoring max values
+    fg.dt_check = 15.         # target time (sec) increment between updating 
                                # max values
-    FG.min_level_check = 5    # which levels to monitor max on
-    FG.arrival_tol = 1.e-2    # tolerance for flagging arrival
-    F.make_fgmax(FG)
+    fg.min_level_check = 5    # which levels to monitor max on
+    fg.arrival_tol = 1.e-2    # tolerance for flagging arrival
+
+    fg.input_file_name = 'fgmax1.txt'
+    fg.write_input_data()
 
 
 if __name__ == "__main__":
