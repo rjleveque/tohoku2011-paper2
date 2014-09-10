dd73f4ccd6a33d438a66f414ee667f6bec831b27
Randy LeVeque <rjl@uw.edu>
2014-09-10 15:46:01 -0400

    use 1/3 sec fgmax grid in HAI1125-26

--- Runs/HAI1125-26/make_fgmax_grid.py
+++ Runs/HAI1125-26/make_fgmax_grid.py
@@ -12,10 +12,10 @@ def make_fgmax_grid1():
     fg.x2 = 204.95
     fg.y1 = 19.715
     fg.y2 = 19.755
-    fg.dx = 1./(3600.)    # 1 arcssecond grid
+    fg.dx = 1./(3*3600.)    # 1/3 arcssecond grid
     fg.tstart_max = 7.8*3600.     # when to start monitoring max values
     fg.tend_max = 1.e10       # when to stop monitoring max values
-    fg.dt_check = 30.         # target time (sec) increment between updating 
+    fg.dt_check = 15.         # target time (sec) increment between updating 
                                # max values
     fg.min_level_check = 6    # which levels to monitor max on
     fg.arrival_tol = 1.e-2    # tolerance for flagging arrival
